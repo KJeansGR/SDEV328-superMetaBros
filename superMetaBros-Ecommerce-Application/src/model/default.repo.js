a5412b1b38ef5ab87929974b54424d2e6fe4145b
WillyxWonka <kalif.edu@gmail.com>
2026-05-16 02:52:32 -0700

    rigged up jank DataBase to verify its possible

--- superMetaBros-Ecommerce-Application/src/model/default.repo.js
+++ superMetaBros-Ecommerce-Application/src/model/default.repo.js
@@ -1,1 +1,8 @@
 //place repository code here...
+import pool from "./db.connect.js";
+
+//read 
+export const getAll = async () => {
+    const [rows] = await pool.query("SELECT * FROM products")
+    return rows;
+}
---
+++ superMetaBros-Ecommerce-Application/src/scripts/seed.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO products
+(itemName, price)
+VALUES
+('Laptop - Dell XPS 13', 999.99),
+('Projector - Epson 2150', 650.00),
+('HDMI Adapter', 19.99),
+('Microphone - Blue Yeti', 129.99),
+('Webcam - Logitech C920', 79.99),
+('Tablet - iPad Air', 599.00),
+('USB-C Hub', 34.99),
+('Portable Speaker', 89.99);
